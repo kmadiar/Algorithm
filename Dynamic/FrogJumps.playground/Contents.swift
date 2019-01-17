f869099bb11cd5bee9cc3559f562870b60d1a114
Kostiantyn Madiar <k.madiar@gmail.com>
2019-01-18 00:38:53 +0200

    frog & missed stones

--- Dynamic/FrogJumps.playground/Contents.swift
+++ Dynamic/FrogJumps.playground/Contents.swift
@@ -3,7 +3,7 @@ import UIKit
 // The frog and the stones
 
 let frogCanJump = 3
-let numberOfStones = 6
+let numberOfStones = 4
 
 func numberOfWays(jumps: Int, numberOfStones: Int) -> Int {
     var a = [1]
@@ -18,3 +18,41 @@ func numberOfWays(jumps: Int, numberOfStones: Int) -> Int {
 }
 
 print(numberOfWays(jumps: frogCanJump, numberOfStones: numberOfStones))
+
+func canOnlyJump(k: Int, numberOfStones: Int) -> Int {
+    var a = [1, 1]
+    
+    for i in 2...numberOfStones {
+        a.append(0)
+        for j in 1...k {
+            if (i % j == 0) {
+                a[i] = a[i] + a[i % j]
+            }
+        }
+    }
+    
+    return a.last!
+}
+
+
+print(canOnlyJump(k: frogCanJump, numberOfStones: numberOfStones))
+
+
+let missedStones = [2]
+
+func numberOfWaysMissedStones(_ missed: [Int], jumps: Int, numberOfStones: Int) -> Int {
+    var a = [1]
+    
+    for i in 1...numberOfStones {
+        a.append(0)
+        if missed.contains(i) {
+            continue
+        }
+        for j in 1...min(i, jumps) {
+            a[i] = a[i] + a[i-j]
+        }
+    }
+    return a.last ?? 0
+}
+
+print(numberOfWaysMissedStones(missedStones, jumps: frogCanJump, numberOfStones: numberOfStones))
