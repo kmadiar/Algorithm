ae1567166911cbc0b46da744994399b883f8e764
Kostiantyn Madiar <k.madiar@gmail.com>
2019-01-18 15:12:18 +0200

    added frog and coins

--- Dynamic/FrogJumps.playground/Contents.swift
+++ Dynamic/FrogJumps.playground/Contents.swift
@@ -3,7 +3,9 @@ import UIKit
 // The frog and the stones
 
 let frogCanJump = 3
-let numberOfStones = 4
+let numberOfStones = 10
+let coins = [0, 2, -3, 1, -2, 1, -1, 1, -1, 1, -1]
+let missedStones = [2]
 
 func numberOfWays(jumps: Int, numberOfStones: Int) -> Int {
     var a = [1]
@@ -37,9 +39,6 @@ func canOnlyJump(k: Int, numberOfStones: Int) -> Int {
 
 print(canOnlyJump(k: frogCanJump, numberOfStones: numberOfStones))
 
-
-let missedStones = [2]
-
 func numberOfWaysMissedStones(_ missed: [Int], jumps: Int, numberOfStones: Int) -> Int {
     var a = [1]
     
@@ -56,3 +55,26 @@ func numberOfWaysMissedStones(_ missed: [Int], jumps: Int, numberOfStones: Int) 
 }
 
 print(numberOfWaysMissedStones(missedStones, jumps: frogCanJump, numberOfStones: numberOfStones))
+
+func frogAndCoins(_ coins: [Int], jumps: Int, numberOfStones: Int) -> Int {
+    var a = [1]
+    
+    for i in 1...numberOfStones {
+        let r = min(i, jumps)
+        a.append(a[i-1] + coins[i])
+        var from = i - 1
+        for j in 1...r {
+            if (a[i]) < (a[i-j] + coins[i]) {
+                a[i] = a[i-j] + coins[i]
+                from = i-j
+            }
+        }
+        
+        print("index \(from)")
+    }
+    return a.last ?? 0
+    
+    return 0
+}
+
+print(frogAndCoins(coins, jumps: frogCanJump, numberOfStones: numberOfStones) )
